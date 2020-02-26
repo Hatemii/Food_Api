a96525033ba4fdd1d53683a3c1e7b9cccc20c26b
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-02-26 14:52:03 +0100

    Setting everything to data with setRecipes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,31 +6,27 @@ const App = () => {
   const APP_ID = "ea3e45dc";
   const APP_KEY = "9b03ca6da51f863cb921a6f581f46be0";
 
-  const [counter,setCounter] = useState(0);
-
-  useEffect(()=> {
-
-  },[]);
+  const [recipes, setRecipes] = useState([]);
 
 
-  const getRecipes = async () =>{
-    const resoponse = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
-    const = data = resoponse.json();
-    console.log(data);
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits)
   }
 
+  return (<div className="App">
 
-  return(
-    <div className="App">
+    <form className="search-form">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">Search</button>
+    </form>
 
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
-        <button className="search-button" type = "submit">Search</button>
-      </form>
-
-  </div>
-  );
+  </div>);
 }
-
 
 export default App;
