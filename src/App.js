c4898fb36539d940d542d92a25efd272aee9c11f
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-02-26 15:01:41 +0100

    Recipe class && loop through recipes data!

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react';
+import Recipe from "./Recipe";
+
 import './App.css';
 
 const App = () => {
@@ -16,8 +18,8 @@ const App = () => {
   const getRecipes = async () => {
     const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
-    setRecipes(data.hits)
-  }
+    setRecipes(data.hits);
+  };
 
   return (<div className="App">
 
@@ -25,6 +27,10 @@ const App = () => {
       <input className="search-bar" type="text"/>
       <button className="search-button" type="submit">Search</button>
     </form>
+
+    {recipes.map(recipe =>(
+      <Recipe />
+     ))};
 
   </div>);
 }
