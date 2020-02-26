444883d519977b238c2825c15e9678bdcf89b3ce
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-02-26 14:22:38 +0100

    Setup API with

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 
 const App = () => {
 
-  const App_Id = "ea3e45dc";
-  const App_Key = "9b03ca6da51f863cb921a6f581f46be0";
-  const exampleReq = "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free";
+  const APP_ID = "ea3e45dc";
+  const APP_KEY = "9b03ca6da51f863cb921a6f581f46be0";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 
   return(
