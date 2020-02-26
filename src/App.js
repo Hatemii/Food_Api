b6691e9e0ab15a1ffb0d65ba4483c9ff1001b3a8
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-02-26 15:35:38 +0100

    Search Box fixed

--- src/App.js
+++ src/App.js
@@ -8,23 +8,40 @@ const App = () => {
   const APP_ID = "ea3e45dc";
   const APP_KEY = "9b03ca6da51f863cb921a6f581f46be0";
 
+
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
+
 
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`);
     const data = await response.json();
     setRecipes(data.hits);
   };
 
-  return (<div className="App">
 
-    <form className="search-form">
-      <input className="search-bar" type="text"/>
+  const updateSearch = e => {
+    setSearch(e.target.value);
+
+  };
+
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
+
+
+  return (<div className="App">
+    <form onSubmit={getSearch} className="search-form">
+      <input className="search-bar" type="text" value={search} onChange={updateSearch} />
       <button className="search-button" type="submit">Search</button>
     </form>
 
