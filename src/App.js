8649e4af6edf3718e595bcb35215bcef923e2e44
finetuning-xh <hatem.xhezairi@finetuning.it>
2020-02-26 14:39:16 +0100

    Api Restructured with new method

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
 const App = () => {
 
   const APP_ID = "ea3e45dc";
   const APP_KEY = "9b03ca6da51f863cb921a6f581f46be0";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [counter,setCounter] = useState(0);
+
+  useEffect(()=> {
+
+  },[]);
+
+
+  const getRecipes = async () =>{
+    const resoponse = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const = data = resoponse.json();
+    console.log(data);
+  }
 
 
   return(
     <div className="App">
-      <h2>Hello</h2>
-    </div>
+
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type = "submit">Search</button>
+      </form>
+
+  </div>
   );
 }
 
